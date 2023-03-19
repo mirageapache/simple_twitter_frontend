fa37c8c016b2ef55d62d5c6bb085c533256f7f21
James <mirageapache@gmail.com>
2023-03-20 00:10:43 +0800

    設定基礎架構

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from 'components/Navbar';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import 'styles/app.css';
 
@@ -6,11 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-            <Navbar />
-        {/* <BrowserRouter>
+        <BrowserRouter>
           <Routes>
+
           </Routes>
-        </BrowserRouter> */}
+        </BrowserRouter>
       </div>
     </div>
   );
