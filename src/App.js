57b65ddffc3d360c0549753c3322a9041c29e31e
kathyhu91 <huchiachun@gmail.com>
2023-03-23 21:01:59 +0800

    fix rout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 // user
 import { RegisterPage, LoginPage, AdminLoginPage, SettingPage } from 'pages';
 import { MainPage } from 'pages';
-
-import FollowPage from "pages/FollowPage.jsx";
-import ProfilePage from 'pages/ProfilePage';
-import AdminPage from 'pages/AdminPage';
 // admin
-// import AdminNavbar from "components/Admin/AdminNavbar.jsx";
-import AdminTweetsPage from'pages/admin/AdminTweetsPage'
-import AdminUsersPage from'pages/admin/AdminUsersPage'
+import AdminPage from 'pages/AdminPage';
 
 export default function App() {
   return (
@@ -19,16 +13,14 @@ export default function App() {
           <Routes>
             <Route path='/register' element={<RegisterPage />} />
             <Route path='/login' element={<LoginPage />} />
-            <Route path='/admin_login' element={<AdminLoginPage />} />
             <Route path='/main' element={<MainPage path='main' />} />
             <Route path='/reply' element={<MainPage path='reply'/>} />
-            <Route path='/setting' element={<SettingPage />} />
-            <Route path='/admin_users' element={<AdminUsersPage />}></Route>
-            <Route path='/admin_tweets' element={<AdminTweetsPage />}></Route>
-            <Route path="/follow" element={<FollowPage />}></Route>
-            <Route path="/profile" element={<ProfilePage />}></Route>
             <Route path="/profile" element={<MainPage path='profile'/>}></Route> 
             <Route path="/follow" element={<MainPage path='follow'/>}></Route>
+            <Route path='/setting' element={<SettingPage />} />
+            <Route path='/admin_login' element={<AdminLoginPage />} />
+            <Route path='/admin_users' element={<AdminPage path='admin_users' />}></Route>
+            <Route path='/admin_tweets' element={<AdminPage path='admin_tweets' />}></Route>
           </Routes>
       </div>
     </div>
