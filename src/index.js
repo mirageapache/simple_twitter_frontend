385a150961353a4ee7946a077f8d063dbb1a62be
James <mirageapache@gmail.com>
2023-03-23 23:52:25 +0800

    add BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import 'styles/base.css'
 import App from "./App.js";
 
 
-
+const basename = process.env.PUBLIC_URL;
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
