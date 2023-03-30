7b317d437775ff7fd73e82bac274ecdd0fc0c18b
James <mirageapache@gmail.com>
2023-03-30 16:51:03 +0800

    test route

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,23 @@
 import { useAuth } from "context/AuthContext";
 import { useEffect } from "react";
-
 import LoadingMes from "components/LoadingMes";
+import { useNavigate } from "react-router";
+
 
 const HomePage = () => {
-  const { logout } = useAuth();
+  // const { logout } = useAuth();
+  const navigate = useNavigate();
+
   useEffect(() => {
-    console.log('homepage execute logout');
-    logout();
-  }, [logout]);
+    // console.log('homepage execute logout');
+    navigate('login')
+    // logout();
+  }, []);
 
   return (
-    <></>
-    // <div>
-    //   <LoadingMes />
-    // </div>
+    <div>
+      <LoadingMes />
+    </div>
   );
 };
 
