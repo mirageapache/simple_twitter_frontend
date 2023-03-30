dfdf42b0781d9ead34fdfe6e7e9e2b07cb6adb23
James <mirageapache@gmail.com>
2023-03-30 12:54:50 +0800

    revise Register notification

--- src/context/NotiContext.jsx
+++ src/context/NotiContext.jsx
@@ -14,7 +14,7 @@ export const useNoti = () => useContext(NotiContext);
 export const NotiProvider = ({ children }) => {
   const [is_alert, setIsAlert] = useState(false); //顯示通知
   const [noti_message, setNotiMessage] = useState({type:'',message:''}); //通知內容
-  const [activeItem, setActiveItem] = useState("main"); //Navbar Item 動態樣式
+  const [activeItem, setActiveItem] = useState("main"); //NavbarItem 動態樣式
   
   return (
     <NotiContext.Provider 
