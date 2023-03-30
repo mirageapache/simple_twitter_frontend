c0c5f082f7ab3b20cb4bc5186382a5e22c792920
James <mirageapache@gmail.com>
2023-03-30 16:32:37 +0800

    fix route

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ export default function App() {
               <NotiProvider>
                 <Routes>
                 <Route path="*" element={<HomePage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="login" element={<LoginPage />} />
                 <Route path="/main" element={<MainPage />}>
                   <Route index element={<MainContent />} />
                   <Route path="main" element={<MainContent />} />
